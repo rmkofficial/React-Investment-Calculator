a5503c9e36212818ca3075418c07b4bb6688387a
kmert0@outlook.com <kmert0@outlook.com>
2024-01-27 23:13:32 +0300

    Add input validation for duration

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     expectedReturn: 6,
     duration: 10,
   });
+  const inputIsValid = userInput.duration >= 1;
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -23,7 +24,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   );
 }
